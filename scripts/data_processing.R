d831b7b493c5a2d40aa99bd4891d40077240d509
caramirezal <cramireza@ciencias.unam.mx>
2020-02-27 23:33:19 +0100

    intratumoral seurat coupled to liger

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -2,49 +2,105 @@
 library(liger)
 source('scripts/funs.R')
 library(Seurat)
+library(SeuratWrappers)
+
+## gene expression + atac integration
+run_liger <- function(liger,
+                      k=10,
+                      liger_path,
+                      plot_path
+                      ){
+        cat('Preprocessing data\n')
+        liger <- normalize(liger)
+        liger <- selectGenes(liger, combine = 'union')
+        liger <- scaleNotCenter(liger)
+        
+        cat('Running liger\n')
+        liger <- optimizeALS(liger, k = k) 
+        liger <- quantileAlignSNF(liger) 
+        
+        cat('Run TSNE\n')
+        liger = runTSNE(liger)
+        
+        cat('Plotting visualizations\n')
+        pdf(plot_path)
+        plotByDatasetAndCluster(liger) #Can also pass in different set of cluster labels to plot
+        plotFeature(liger, "nUMI")
+        plotWordClouds(liger)
+        plotGeneLoadings(liger)
+        dev.off()
+
+        cat('Saving results\n')
+        saveRDS(liger, liger_path)
+}
+
+save_liger <- function(ligerex,
+                       dir_path = '.') {
+        cat('Creating list object\n')
+        liger_res <- list(
+                H=ligerex@H,
+                cell_data=ligerex@cell.data,
+                H_norm=ligerex@H.norm,
+                W=ligerex@W,
+                V=ligerex@V,
+                tsne_coords=ligerex@tsne.coords,
+                alignment_clusters=ligerex@alignment.clusters,
+                clusters=ligerex@clusters)
+        
+        cat('Saving results\n')
+        saveRDS(liger_res, dir_path)
+}
+
+run_liger_seurat <- function(seurat){
+     seurat.p <- NormalizeData(seurat)
+     seurat.p <- FindVariableFeatures(seurat.p)
+     seurat.p <- ScaleData(seurat.p, split.by = "orig.ident", do.center = FALSE)
+     seurat.p <- RunOptimizeALS(seurat.p, k = 20, lambda = 5, split.by = "orig.ident")
+     seurat.p <- RunQuantileAlignSNF(seurat.p, split.by = "orig.ident")
+     seurat.p <- RunUMAP(seurat.p, dims = 1:ncol(seurat.p[["iNMF"]]), reduction = "iNMF")
+     saveRDS(seurat.p, 'data/intratumotal_liger_seurat.rds')
+}
+
+#############################################################################
+##   									   ##
+##            Adrenal medula batch correction 				   ##
+##									   ##
+#############################################################################
+
+#cat('Loading data\n')
+#file_names <- list.files('data/adrenal_medulla', full.names=TRUE)
+#f_names <- gsub('data/adrenal_medulla/', '', file_names)
+#f_names <- gsub('_.*', '', f_names)
+#f_names
+#counts <- lapply(file_names, readRDS)
+#names(counts) <- f_names
+#sapply(counts, class)
+#sapply(counts, dim)
+
+#cat('Creating liger object\n')
+#liger <- createLiger(counts)
+
+#cat('Run liger\n')
+#run_liger(
+#     liger = liger,
+#     k = 10,
+#     liger_path = 'data/batch_correction_adrenal_medulla.rds',
+#     plot_path = 'figures/batch_correction.pdf'
+#)
 
 
+#liger <- readRDS('data/batch_correction_adrenal_medulla.rds')
+#save_liger(liger, 'data/batch_correction_adrenal_medulla_tsne.rds')
 
-cat('Loading data\n')
-file_names <- list.files('data/adrenal_medulla', full.names=TRUE)
-f_names <- gsub('data/intratumoral/', '', file_names)
-f_names <- gsub('\\.rds', '', f_names)
-counts <- lapply(file_names, readRDS)
 
-#cat('Creating Seurat object\n')
-#counts_seu <- merge(counts[[1]], counts[2:length(counts)], add.cell.ids = f_names, project = "neuroblastoma")
-#merged_dir <- 'data/merged_seurat.rds'
-#cat('Saving results to', merged_dir,'\n')
-#saveRDS(counts_seu, merged_dir)
+########################################################################
+##  								      ##
+##                 Intratumoral                                       ##
+## 								      ##
+########################################################################
 
-merged_seu <- readRDS('data/merged_seurat.rds')
-merged_seu
-cat('Creating liger object \n')
-ligerex <- NormalizeData(merged_seu)
+cat('Reading intratumoral data\n')
+seurat <- readRDS('data/intratumoral_merged_seurat.rds')
 
-#cat('Preprocessing \n')
-#ligerex <- normalize(ligerex)
-#ligerex <- selectGenes(ligerex, combine = 'union')
-#ligerex <- scaleNotCenter(ligerex)
-
-#cat('Running liger\n')
-#ligerex <- optimizeALS(ligerex, k = 10) 
-#ligerex <- quantileAlignSNF(ligerex) 
-                
-#cat('Run TSNE\n')
-#ligerex = runTSNE(ligerex)
-
-#cat('Saving results\n')
-#saveRDS(ligerex, 'data/batch_correction.rds')
-
-#ligerex <- readRDS('data/batch_correction.rds')
-
-#cat('Plotting visualizations\n')
-#pdf('figures/batch_correction.pdf')
-#plotByDatasetAndCluster(ligerex) #Can also pass in different set of cluster labels to plot
-#plotFeature(ligerex, "nUMI")
-#plotWordClouds(ligerex)
-#plotGeneLoadings(ligerex)
-#dev.off()
-
-#liger_save(ligerex, 'data/batch_correction_sum.rds')
+cat('Running liger over merged seurta object\n')
+run_liger_seurat(seurat)
